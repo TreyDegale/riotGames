7f75662b085db771020b470e0df9eda49540cd02
TreyDegale <treydegale98@gmail.com>
2023-04-29 16:36:47 +0100

    update status code expectation

--- flask_app/test_scripts.py
+++ flask_app/test_scripts.py
@@ -5,4 +5,5 @@ def test_getChampionData():
     assert retrieveChampionData.gangplank['name'] == "Gangplank"
 
 def test_riotGames():
-    assert riotGames.response.status_code == 200
+    # Test for 403 response - key updates every 24 hours - can't test for 200 response
+    assert riotGames.response.status_code == 403
