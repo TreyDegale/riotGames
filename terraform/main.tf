52da4bd99c21e24250035fbde2d16d2040f57490
TreyDegale <treydegale98@gmail.com>
2023-05-02 12:56:48 +0100

    underscore --> hyphen

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "aws_lightsail_container_service" "flask_app" {
 
 resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
   container {
-    container_name = "flask_app"
+    container_name = "flask-app"
 
     image = "treydegale/flask_app:0.0.2"
     
